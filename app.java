f0942c983a2d50b9edb766322da67662cca54723
eausergit <96477529+eausergit@users.noreply.github.com>
2026-04-30 21:36:20 +0100

    Create app.java

---
+++ app.java
@@ -0,0 +1,6 @@
+public class app {
+	public static void main(String[] args) {
+		String password = "admin123"; // hardcoded credentials example
+		System.out.println("Password is: " + password);
+	}
+}
